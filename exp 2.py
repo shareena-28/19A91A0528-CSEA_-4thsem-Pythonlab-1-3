bfca56097ea086d57102e44b4786da47dcb3703b
Shareena nirmala. K <84276084+shareena-28@users.noreply.github.com>
2021-05-22 12:35:14 +0530

    Create exp 2.py

---
+++ exp 2.py
@@ -0,0 +1,20 @@
+"""
+2.1-Implement a python Script to compute distance between two points taking inp from the user (pythagorean Theorem)
+"""
+x1=int(input("enter x1 : "))
+
+x2=int(input("enter x2 : "))
+
+y1=int(input("enter y1 : "))
+
+y2=int(input("enter y2 : "))
+
+result= ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5)
+
+print("distance between",(x1,y1),"and",(x2,y2),"is : ",result)
+#output
+enter x1 : 5
+enter x2 : 6
+enter y1 : 7
+enter y2 : 8
+distance between (5,7) and (6,8) is : 1.4142135623730951
